876a3e82d086681face88be37b7d2d79146b1744
kevincychen <36891526+kevincychen@users.noreply.github.com>
2024-02-15 23:41:59 +0800

    Project springboot-forex-mongodb Init

---
+++ springboot-forex-mongodb/src/main/java/com/wits/demo/SpringbootForexMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.wits.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootForexMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootForexMongodbApplication.class, args);
+	}
+
+}
---
+++ springboot-forex-mongodb/src/test/java/com/wits/demo/SpringbootForexMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wits.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootForexMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
