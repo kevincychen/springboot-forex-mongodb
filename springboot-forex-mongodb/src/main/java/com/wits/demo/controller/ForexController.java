629cfff2b4d64b3d5cbda1cdebe9cda0dd0d0f43
kevincychen <36891526+kevincychen@users.noreply.github.com>
2024-02-17 00:14:15 +0800

    springboot-forex-mongodb: 重構「1. 呼叫 API，取得外匯成交資料後insert到DB」

--- springboot-forex-mongodb/src/main/java/com/wits/demo/controller/ForexController.java
+++ springboot-forex-mongodb/src/main/java/com/wits/demo/controller/ForexController.java
@@ -3,22 +3,25 @@ package com.wits.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wits.demo.service.ForexService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping(path = "/forex")
+@Tag(name = "外匯查詢平台：Forex")
 public class ForexController {
 
 	@Autowired
 	ForexService forexService;
 
-	@Tag(name = "1. 呼叫 API，取得外匯成交資料後insert到DB")
-	@RequestMapping(value = "/getJsonByCallingApiThenInsertDB", method = RequestMethod.GET)
+	@Operation(summary = "1. 呼叫 API，取得外匯成交資料後insert到DB", description = "1. 呼叫 API，取得外匯成交資料後insert到DB")
+	@GetMapping(path = "/getJsonByCallingApiThenInsertDB")
 	public void getJsonByCallingApiThenInsertDB() {
 		List<Object> forexDataList = forexService.getForexData();
 		forexService.insertToMongoDb(forexDataList);
