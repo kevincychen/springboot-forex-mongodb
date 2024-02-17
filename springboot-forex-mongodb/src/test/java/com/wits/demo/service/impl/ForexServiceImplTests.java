3bdd1f763ea74414b53f409d449f53a77e919c3e
kevincychen <36891526+kevincychen@users.noreply.github.com>
2024-02-18 02:15:56 +0800

    springboot-forex-mongodb: Ready for JUnit Tests

---
+++ springboot-forex-mongodb/src/test/java/com/wits/demo/service/impl/ForexServiceImplTests.java
@@ -0,0 +1,29 @@
+package com.wits.demo.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ForexServiceImplTests {
+
+	@Test
+	void testGetForexData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertToMongoDb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidateQueryRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQueryDb() {
+		fail("Not yet implemented");
+	}
+
+}
